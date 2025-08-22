2001c2f6ad9f757e5efc111cb10ae1ab00e177c5
LucasOsella <lucasosella01@gmail.com>
2025-08-21 23:00:11 -0300

    Comit Inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Tp_inmobiliaria.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
