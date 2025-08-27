461f64ad94711791b12fcb52cd94538f2bbc4e20
LucasOsella <lucasosella01@gmail.com>
2025-08-26 22:10:09 -0300

    metodos Agregar, Editar inquilinos

--- Controllers/InquilinosControllers.cs
+++ Controllers/InquilinosControllers.cs
@@ -4,7 +4,7 @@ using Tp_inmobiliaria.Models;
 namespace Tp_inmobiliaria.Controllers;
 
 public class InquilinosController : Controller
-{   
+{
     private readonly ILogger<InquilinosController> _logger;
     private RepositoriosInquilinos repo;
 
@@ -19,5 +19,31 @@ public class InquilinosController : Controller
         var lista = repo.ObtenerInquilinos();
         return View(lista); // Busca Views/Inquilinos/Index.cshtml
     }
+
+    public IActionResult AgregarInquilino()
+    {
+        return View(); // Busca Views/Inquilinos/agregarInquilino.cshtml
+    }
+
+       public IActionResult EditarInquilino(int id)
+    {
+        var inquilinos = repo.obtenerInquilino(id);
+        return View(inquilinos);// Busca Views/Propietarios/editarInquilino.cshtml
+    }
+
+    //Guarda el inquilino desde la visa AgregarInquilino
+    public IActionResult GuardarInquilino(Inquilinos inquilinos)
+    {
+        repo.agregarInquilino(inquilinos);
+        return RedirectToAction("Index");
+    }
+
+    //Guarda el inquilino editado desde la vista Editar Inquilino.
+    public IActionResult GuardarEdicionInquilinos(Inquilinos inquilinos)
+    {
+        repo.editarInquilino(inquilinos);
+        return RedirectToAction("Index");
+    }
+
 }
 
--- Models/RepositoriosInquilinos.cs
+++ Models/RepositoriosInquilinos.cs
@@ -37,5 +37,71 @@ public class RepositoriosInquilinos
         }
         return inquilinos;
     }
+
+    public void agregarInquilino(Inquilinos inquilino)
+    {
+        var query = @"INSERT INTO inquilino (dni, nombre, telefono, email, direccion) VALUES (@dni, @nombre, @telefono, @email, @direccion)";
+        using (MySql.Data.MySqlClient.MySqlConnection connection = new MySql.Data.MySqlClient.MySqlConnection(ConnetionString))
+        {
+            using (MySql.Data.MySqlClient.MySqlCommand command = new MySql.Data.MySqlClient.MySqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@dni", inquilino.Dni);
+                command.Parameters.AddWithValue("@nombre", inquilino.Nombre);
+                command.Parameters.AddWithValue("@telefono", inquilino.Telefono);
+                command.Parameters.AddWithValue("@email", inquilino.Email);
+                command.Parameters.AddWithValue("@direccion", inquilino.Direccion);
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+
+    public void editarInquilino(Inquilinos inquilino)
+    {
+        var query = @"UPDATE inquilino SET dni=@dni, nombre=@nombre, telefono=@telefono, email=@email, direccion=@direccion WHERE id=@id";
+        using (MySql.Data.MySqlClient.MySqlConnection connection = new MySql.Data.MySqlClient.MySqlConnection(ConnetionString))
+        {
+            using (MySql.Data.MySqlClient.MySqlCommand command = new MySql.Data.MySqlClient.MySqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@id", inquilino.Id);
+                command.Parameters.AddWithValue("@dni", inquilino.Dni);
+                command.Parameters.AddWithValue("@nombre", inquilino.Nombre);
+                command.Parameters.AddWithValue("@telefono", inquilino.Telefono);
+                command.Parameters.AddWithValue("@email", inquilino.Email);
+                command.Parameters.AddWithValue("@direccion", inquilino.Direccion);
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+
+    public Inquilinos obtenerInquilino(int id)
+    {
+        var query = "SELECT id, dni, nombre, telefono, email, direccion FROM inquilino WHERE id=@id";
+        Inquilinos inquilino = null;
+
+        using (var connection = new MySql.Data.MySqlClient.MySqlConnection(ConnetionString))
+        using (var command = new MySql.Data.MySqlClient.MySqlCommand(query, connection))
+        {
+            command.Parameters.AddWithValue("@id", id);
+            connection.Open();
+            using (var reader = command.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    inquilino = new Inquilinos()
+                    {
+                        Id = reader.GetInt32("id"),
+                        Dni = reader.GetInt32("dni"),
+                        Nombre = reader.GetString("nombre"),
+                        Telefono = reader.GetString("telefono"),
+                        Email = reader.GetString("email"),
+                        Direccion = reader.GetString("direccion")
+                    };
+                }
+            }
+        }
+        return inquilino;
+    }
     
 }
